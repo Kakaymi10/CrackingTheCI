c4c7f9afecbf2e41774214cca244ea8f9a5969fd
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-06-29 17:42:37 +0200

    Update removeDups.py

--- LinkedList/removeDups.py
+++ LinkedList/removeDups.py
@@ -8,19 +8,69 @@ In the below solution, we simply iterate through the linked list, adding each el
 we discover a duplicate element, we remove the element and continue iterating. We can do this all in one 
 pass since we are using a linked list. """
 
-def delete_duplicates(head):
-    if head is None:
-        return
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.current = None
+    def prepend(self, data):
+        new_data = Node(data)
+        if self.head is None:
+            self.head = new_data
+        new_data.next = self.head
+        self.head = new_data
+        current = self.head
 
-    seen_values = set()
-    previous = None
-    current = head
-
-    while current is not None:
-        if current.data in seen_values:
-            previous.next = current.next
+    def append(self, data):
+        new_data = Node(data)
+        if self.head is None:
+            self.head = new_data
         else:
-            seen_values.add(current.data)
-            previous = current
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_data
+        
+    def display(self):
+        elements = []
+        current = self.head
+        while current is not None:
+            elements.append(current.data)
+            current = current.next
+        print("Linked List:", elements)
+        
+    def removeDups(self):
+        if self.head == None:
+            return
+        HashSet = set()
+        previous = None
+        
+        current = self.head
+        while current is not None:
+            if current.data in HashSet:
+                previous.next = current.next
+            else:
+                HashSet.add(current.data)
+                previous = current
+                
+            current = current.next
+            
+            
+node = LinkedList()
+node.append(1)
+node.append(2)
+node.append(3)
+node.append(4)
+node.append(1)
+node.prepend(7)
+node.prepend(2)
+node.prepend(4)
+node.prepend(8)
 
-        current = current.next
+node.display()
+node.removeDups()
+node.display()
